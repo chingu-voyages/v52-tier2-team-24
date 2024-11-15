d55f0aa00fa7420adbb8101d7921be88ebfc1fb4
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-11-15 12:00:00 -0500

    using react hook form i am now returning object

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-
-
-import './App.css'
-import LandingForm from './components/LandingForm'
-
+import "./App.css";
+import LandingForm from "./components/LandingForm";
+import TestValidate from "./components/TestValidate";
 
 function App() {
-
-
   return (
     <>
-  <div className="min-h-screen bg-blue-100 flex justify-center items-center">
-      {/* <h1 className="text-4xl font-bold text-gray-800">Hello from Solar App!</h1> */}
-      <LandingForm />
-    </div>
+      <div className="min-h-screen bg-blue-100 flex justify-center items-center">
+        {/* <h1 className="text-4xl font-bold text-gray-800">Hello from Solar App!</h1> */}
+        <div className="flex gap-4">
+          <LandingForm />
+          <TestValidate />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TestValidate.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function TestValidate() {
+
+  const {register, handleSubmit} = useForm();
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+
+    const value = e.target.value;
+
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(inputs);
+  //   setInputs("")
+  // };
+
+  return (
+    <div className="w-1/2 h-[90vh] flex flex-col gap-4 items-center justify-between  bg-slate-50 pt-8">
+      <header className="text-center">
+        <h1 className="text-8xl text-green-800 mb-4 font-extrabold">
+          Solar Title
+        </h1>
+        <p>Basic info about the app in subtitle under web name</p>
+      </header>
+
+      <div className="border-2 w-cd  h-contain  items-center border-slate-300 rounded-md bg-green-100">
+        <h1 className="text-center text-4xl my-10  text-green-800 font-bold underline">
+          Request an Appointment
+        </h1>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log("valdata:", data)
+          })}
+          className="flex flex-col gap-4 items-start ml-5"
+        >
+          <div className="flex gap-3">
+            <input
+              className="border border-slate-300 rounded-lg pl-2"
+              type="text"
+              // register is a callback function that will return props and inject into inputs
+             {...register("firstName")}
+              placeholder="First Name"
+              value={inputs.firstName || ""}
+              onChange={handleChange}
+            />
+            <input
+              className=" border border-slate-300 rounded-lg pl-2"
+              type="text"
+              {...register("lastName")}
+              placeholder="Last Name"
+              value={inputs.lastName || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <input
+            className=" border border-slate-300 rounded-lg pl-2 "
+            type="text"
+            {...register("email")}
+            placeholder="Email"
+            value={inputs.email || ""}
+            onChange={handleChange}
+          />
+          {/* Will this be where auto-complete component or section will go? */}
+          <input
+            className=" border border-slate-300 rounded-lg pl-2"
+            type="text"
+            {...register("address")}
+            placeholder="Address"
+            value={inputs.address || ""}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="bg-slate-800 rounded-lg text-slate-50 text-md py-2 px-4 ml-auto m-4"
+          >
+            Select a time{" "}
+          </button>
+        </form>
+      </div>
+      <p className="mb-4 text-lg">
+        City Hall Employee?{" "}
+        <a className="underline text-blue-500" href="/">
+          Sign In
+        </a>
+      </p>
+    </div>
+  );
+}
