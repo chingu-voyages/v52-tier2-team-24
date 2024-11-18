e7adfe709fabf132ebcb5e3f37edc760ce31ce4e
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-11-18 15:10:03 -0500

    aligned to match figma design and page is resonsive added cancel button for form reset functionality form is fully responsive on desktop and mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LandingForm from "./components/LandingForm";
 function App() {
   return (
     <>
-      <div className=" bg-blue-100 flex justify-center items-center">
+      <div className=" flex justify-center items-center">
         {/* <h1 className="text-4xl font-bold text-gray-800">
           Hello from Solar App!
         </h1> */}
--- src/components/LandingForm.jsx
+++ src/components/LandingForm.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { GiSolarPower } from "react-icons/gi";
 // import { object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -35,74 +36,95 @@ export default function TestValidate() {
 
   return (
     <div className="w-full  flex flex-col gap-4 items-start justify-between   pt-8">
-      <header className="h-[20vh]">
-        <h1 className="~text-3xl/6xl mb-4 font-extrabold">
-          Schedule an Appointment
-        </h1>
-        <p>Please fill out the form to request and appointment</p>
+      <header className="h-[20vh] flex items-center gap-5 ">
+        <GiSolarPower className="w-28 h-28" />
+        <div>
+          <h1 className="text-2xl mb-4 font-extrabold">
+            Schedule an Appointment
+          </h1>
+          <p>Please fill out the form to request and appointment</p>
+        </div>
       </header>
 
-      <div className="border-2 w-full  py-10  items-center flex flex-col gap-8 justify-center border-slate-300 rounded-md bg-green-100">
+      <div className="border-2 w-full  py-10  items-center flex flex-col justify-center border-slate-300 rounded-md">
         <form
           onSubmit={handleSubmit((data) => {
             handleFormSubmit(data);
           })}
           className=" sm:ml-5 sm:items-start flex text-lg  flex-col w-11/12 mx-auto"
         >
-          <div className="sm:flex-row md:w-1/2   flex-grow w-full flex flex-col  ">
-            <p className="ml-2 font-bold">First Name *</p>
-            <input
-              className={` pl-2 border mb-4  py-2 rounded-lg  ${
-                errors.firstName
-                  ? "border-red-500 placeholder-red-500"
-                  : "border-slate-300"
-              }`}
-              type="text"
-              {...register("firstName")}
-              placeholder={errors.firstName?.message || "Enter Your Name"}
-            />
-            <p className="ml-2 font-bold">Last Name *</p>
-            <input
-              className={`border mb-4   rounded-lg pl-2 py-2  ${
-                errors.lastName
-                  ? "border-red-500 placeholder-red-500"
-                  : "border-slate-300"
-              }`}
-              type="text"
-              {...register("lastName")}
-              placeholder={errors.lastName?.message || "Enter Your Last Name"}
-            />
+          <div className="sm:flex-row gap-2  flex-grow w-full flex flex-col   ">
+            <div className="sm:w-1/2 ">
+              <p className="ml-2 font-bold mb-2">First Name *</p>
+              <input
+                className={`w-full pl-2 border mb-4  py-2 rounded-lg   ${
+                  errors.firstName
+                    ? "border-red-500 placeholder-red-500"
+                    : "border-slate-400"
+                }`}
+                type="text"
+                {...register("firstName")}
+                placeholder={errors.firstName?.message || "Enter Your Name"}
+              />
+            </div>
+            <div className="sm:w-1/2 ">
+              <p className="ml-2 font-bold mb-2">Last Name *</p>
+              <input
+                className={`w-full  border mb-4   rounded-lg pl-2 py-2  ${
+                  errors.lastName
+                    ? "border-red-500 placeholder-red-500"
+                    : "border-slate-400"
+                }`}
+                type="text"
+                {...register("lastName")}
+                placeholder={errors.lastName?.message || "Enter Your Last Name"}
+              />
+            </div>
           </div>
-          <div className="sm:flex-row md:w-1/2 flex-grow w-full flex flex-col  ">
-            <p className="ml-2 font-bold">Email *</p>
-            <input
-              className={` pl-2 py-2 mb-4   border rounded-lg  ${
-                errors.email
-                  ? "border-red-500 placeholder-red-500"
-                  : "border-slate-300"
-              }`}
-              type="text"
-              {...register("email")}
-              placeholder={errors.email?.message || "Enter Your  Email"}
-            />
-            <p className="ml-2 font-bold">Address</p>
-            <input
-              className={` pl-2 py-2 border mb-4   rounded-lg  ${
-                errors.address
-                  ? "border-red-500 placeholder-red-500"
-                  : "border-slate-300 "
-              }`}
-              type="text"
-              {...register("address")}
-              placeholder={errors.address?.message || "Enter Your Address"}
-            />
+
+          <div className="sm:flex-row gap-2 flex-grow w-full flex flex-col  ">
+            <div className="sm:w-1/2 ">
+              <p className="ml-2 font-bold mb-2">Email *</p>
+              <input
+                className={`w-full  pl-2 py-2 mb-4    border rounded-lg  ${
+                  errors.email
+                    ? "border-red-500 placeholder-red-500"
+                    : "border-slate-400"
+                }`}
+                type="text"
+                {...register("email")}
+                placeholder={errors.email?.message || "Enter Your  Email"}
+              />
+            </div>
+            <div className="sm:w-1/2 ">
+              <p className="ml-2 font-bold mb-2">Address</p>
+              <input
+                className={`w-full  pl-2 py-2 border mb-4   rounded-lg  ${
+                  errors.address
+                    ? "border-red-500 placeholder-red-500"
+                    : "border-slate-400 "
+                }`}
+                type="text"
+                {...register("address")}
+                placeholder={errors.address?.message || "Enter Your Address"}
+              />
+            </div>
           </div>
-          <p className="ml-2 font-bold">Preferred Timeslot</p>
+
+          <p className="ml-2 font-bold mb-2">Preferred Timeslot</p>
           <div className="sm:flex-row sm:w-1/2 flex flex-col w-full gap-2  ">
-            <select className="sm:w-1/2 py-3 rounded-lg" name="" id="0"></select>
-            <select className="sm:w-1/2 py-3 rounded-lg" name="" id=""></select>
+            <select
+              className="sm:w-1/2 py-3 rounded-lg bg-slate-300 border border-slate-400"
+              name=""
+              id="0"
+            ></select>
+            <select
+              className="sm:w-1/2 py-3 rounded-lg bg-slate-300 border border-slate-400"
+              name=""
+              id=""
+            ></select>
           </div>
-          <div className="sm:flex-row sm:w-1/2 sm:mx-auto flex flex-col gap-2 mt-5">
+          <div className="sm:flex-row sm:w-1/2 sm:mx-auto sm:mt-10  flex flex-col gap-2  mt-5">
             <button
               type="submit"
               className=" w-full font-bold  bg-green-600 rounded-lg text-slate-50 text-xl  py-4 "
@@ -110,6 +132,7 @@ export default function TestValidate() {
               Submit{" "}
             </button>
             <button
+            type="button"
               onClick={() => reset()}
               className=" border-2 border-slate-600  w-full  rounded-lg  text-xl font-bold uppercase py-4 "
             >
