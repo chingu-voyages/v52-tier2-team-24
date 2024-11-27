6067c6dcee35741c30471fcec645203e5b18af59
lkallen87 <159066551+lkallen@users.noreply.github.com>
2024-11-27 17:17:41 -0500

    login modal: added functionality, login auth, styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,43 @@
 import { Button } from "./Button";
-import logo from '../images/sun.png';
-import { useLocation, useNavigate } from 'react-router-dom';
+import logo from "../images/sun.png";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import LoginModal from "./LoginModal";
 
 export const NavBar = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const isAdminRoute = location.pathname === '/admin';
+  const isAdminRoute = location.pathname === "/admin";
+
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
 
   const handleClick = () => {
     if (isAdminRoute) {
-      navigate('/');
+      navigate("/");
     } else {
-      navigate('/admin');
+      setIsLoginOpen(!isLoginOpen);
     }
   };
+
+  function handleClose() {
+    setIsLoginOpen(!isLoginOpen);
+  }
+
   return (
-    <div id='navbar' className='flex justify-between w-full h-18 px-10 md:px-20 lg:px-10 py-6'> 
-      <div className='flex items-center min-w-44'> 
-        <img src={logo} className='size-7 mr-2'/>
-        <p className='text-2xl'>Solar Panel App</p>
+    <div
+      id="navbar"
+      className="flex justify-between w-full h-18 px-10 md:px-20 lg:px-10 py-6"
+    >
+      <div className="flex items-center min-w-44">
+        <img src={logo} className="size-7 mr-2" />
+        <p className="text-2xl">Solar Panel App</p>
       </div>
       <Button
-  text={isAdminRoute ? "Log Out" : "Sign In"}
-  isTransparent={!isAdminRoute}
-  onClick={handleClick}
-/>
+        text={isAdminRoute ? "Log Out" : "Sign In"}
+        isTransparent={!isAdminRoute}
+        onClick={handleClick}
+      />
+      <LoginModal isLoginOpen={isLoginOpen} handleClose={handleClose} />
     </div>
-  )
-}
+  );
+};
