ace2162935d2b10b9a0071e8583d8f8a3b3f551f
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-13 14:39:10 -0500

    added carousel and case if no new appointmnets are present made all responsive

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, NavLink, Outlet } from "react-router-dom";
-import calendar from "../images/calendar.png";
-import { formatAddress } from "../helpers/formatAddress";
+
+import AppointmentCarousel from "./AppointmentCarousel";
 
 const AdminPage = () => {
   const navigate = useNavigate();
@@ -70,65 +70,18 @@ const AdminPage = () => {
 
   return (
     <div className="min-h-screen bg-white">
-      {/* NEW APPOINTMENTS SECTION */}
-      <div className="">
-        <h2 className="text-lg font-bold my-4 sm:ml-2 text-center sm:text-start underline">
-          New Appointment Requests
-        </h2>
-        <div className="flex flex-wrap mb-12 h-[200px] justify-center items-center">
-          {requests.length === 0 ? (
-            <p className="text-gray-500 text-center">
-              No new appointment requests.
-            </p>
-          ) : (
-            requests.map((request) => (
-              <div
-                key={request.id}
-                className="flex items-start gap-4 bg-white p-2 min-w-[250px]"
-              >
-                <div className="flex flex-col items-center ">
-                  <div className="flex items-center justify-center  bg-gray-100 rounded-full h-[55px] w-[55px] mb-3">
-                    <img
-                      src={calendar}
-                      alt="Calendar"
-                      className="h-[40px] w-[40px]"
-                    />
-                  </div>
-                  <h3 className="text-gray-700 text-lg font-bold ">
-                    {request.firstName}
-                  </h3>
-                  <h3 className="text-gray-700 text-lg font-bold ">
-                    {request.lastName}
-                  </h3>
-                  <p className="text-gray-500 text-sm text-center">
-                    {request.date}
-                  </p>
-                  <p className="text-gray-500 text-sm mb-2 text-center">
-                    {request.time}
-                  </p>
-                  <p className="font-medium text-md text-center">
-                    {formatAddress(request.address)}
-                  </p>
-                  <div className="flex gap-4 items-center mt-2">
-                    <button
-                      className="w-10 h-10 flex items-center justify-center rounded-full bg-green-500 text-white hover:bg-green-600 transition-colors"
-                      onClick={() => handleApprove(request.id)}
-                    >
-                      ✓
-                    </button>
-                    <button
-                      className="w-10 h-10 flex items-center justify-center rounded-full bg-red-500 text-white hover:bg-red-600 transition-colors"
-                      onClick={() => handleCancel(request.id)}
-                    >
-                      ✕
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-      </div>
+      <h2 className="text-lg font-bold my-4 sm:ml-2 text-center sm:text-start underline">
+        New Appointment Requests
+      </h2>
+      {requests.length === 0 ? (
+        <p className="text-red-500 text-center mb-5">No new appointments.</p>
+      ) : (
+        <AppointmentCarousel
+          requests={requests}
+          handleApprove={handleApprove}
+          handleCancel={handleCancel}
+        />
+      )}
 
       <div>
         <div className="flex justify-center gap-2 sm:justify-start mb-2 border-b-2 border-gray-300">
